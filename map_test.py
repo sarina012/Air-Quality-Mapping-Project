a7484b87a1511417cbed8465d494b31acf1cbb31
amolina2 <97627060+amolina2@users.noreply.github.com>
2023-12-05 16:20:56 -0800

    Add files via upload

--- map_test.py
+++ map_test.py
@@ -22,8 +22,8 @@ def process_air_quality_data(air_quality_file, coordinates_file):
     air_quality_df = pd.DataFrame(air_quality_data)
     coord_data = pd.read_excel(coordinates_file, engine='openpyxl')
 
-    if coord_data.empty:
-        print("The coordinates data is empty. Please check your Excel file.")
+    if len(coord_data) < 14:
+        print("There are not enough coordinate points. Please ensure there are at least 14 coordinate points.")
         return
 
     first_coord = coord_data.iloc[0]
@@ -31,39 +31,40 @@ def process_air_quality_data(air_quality_file, coordinates_file):
 
     prev_time = None
     batch_data = []
-    batch_index = 0
+    batch_index = 0  # Keep track of which batch we are on
 
     for index, row in air_quality_df.iterrows():
-        if prev_time and (row['Date/Time'] - prev_time).total_seconds() > 120:
-            # Time gap detected, process the current batch
+        time_diff = (row['Date/Time'] - prev_time).total_seconds() / 60 if prev_time else 0
+
+        if time_diff > 2 or index == 0:  # Move to the next location every 2 minutes or at the start.
             if batch_data:
                 avg_pm25, avg_pm10 = calculate_averages(pd.DataFrame(batch_data))
-                coord = coord_data.iloc[batch_index % len(coord_data)]
+                coord = coord_data.iloc[batch_index % len(coord_data)]  # Loop over if index exceeds
                 folium.Marker(
                     location=[coord['Latitude'], coord['Longitude']],
                     tooltip=f"Batch {batch_index}: PM2.5 = {avg_pm25:.2f}, PM10 = {avg_pm10:.2f}",
                     icon=folium.Icon(color='blue')
                 ).add_to(m)
-                batch_data = []  # Start new batch
-                batch_index += 1
+                batch_data = []  # Clear batch data for the next batch.
+                batch_index += 1  # Increment batch index.
 
-            # If the gap is greater than 1 hour, reset the batch index
-            if (row['Date/Time'] - prev_time).total_seconds() > 3600:
-                batch_index = 0
+        if time_diff > 30:  # If gap is more than 30 minutes, reset the batch index.
+            batch_index = 0
 
         batch_data.append(row)
         prev_time = row['Date/Time']
 
+    # Process the final batch
     if batch_data:
         avg_pm25, avg_pm10 = calculate_averages(pd.DataFrame(batch_data))
-        coord = coord_data.iloc[batch_index % len(coord_data)]
+        coord = coord_data.iloc[batch_index % len(coord_data)]  # Ensure we don't exceed the coordinates count.
         folium.Marker(
             location=[coord['Latitude'], coord['Longitude']],
             tooltip=f"Final Batch: PM2.5 = {avg_pm25:.2f}, PM10 = {avg_pm10:.2f}",
             icon=folium.Icon(color='blue')
         ).add_to(m)
 
-    m.save('air_quality_map.html')
+    m.save('air_quality_map.html')  # Save the map in a writable directory
 
 # Example usage
 process_air_quality_data('DylosLog.txt', 'coordinates.xlsx')
